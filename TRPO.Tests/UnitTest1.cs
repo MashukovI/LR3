8df0c34f2b734548235ee8361ec50a74ec270ced
User <User@HOME>
2023-12-28 05:38:54 +0500

    Добавьте файлы проекта.

---
+++ TRPO.Tests/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace TRPO.Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
